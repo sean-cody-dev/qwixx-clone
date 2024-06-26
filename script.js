84bd9e60d72e5ab19895f8b3311761e4ae0c6998
sean-cody-dev <meghancody@Meghans-MacBook.local>
2024-06-26 11:55:11 -0400

    one player mvp complete

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const diceContainer = document.getElementById('dice-container');
 
 // game parameters
 const gameColors = ['red', 'yellow', 'green', 'blue'];
+const whiteDiceNames = ['whiteOne', 'whiteTwo'];  // To store the names of white dice
 
 // game state
 let activeDice = {};
@@ -20,9 +21,21 @@ let penaltyCount = 0;
 function rollDice() {
     return diceValue = Math.floor(Math.random()*6)+1;
 }
-// shows dice roll
+
+// rolls all active dice
+function turnRoll() {
+    Object.keys(activeDice).forEach((diceColor) => {
+        if (activeDice[diceColor]) {
+            currentDiceRoll[diceColor] = rollDice();
+        }
+    })
+};
+
+// Updates DOM with Dice Roll Values
 function showDiceRoll() {
     const colors = Object.keys(currentDiceRoll); // Get all color keys from the scoreOptions object
+    
+    // Map dice roll values to text strings for icon classes
     const valueMap = {
         1: 'one',
         2: 'two',
@@ -41,7 +54,6 @@ function showDiceRoll() {
         }
         });
 
-    // TODO: make this code DRYer
     // get dice icon for white dice
     const whiteOneDice = document.getElementById('dice-white-one');
     const whiteTwoDice = document.getElementById('dice-white-two');
@@ -49,6 +61,46 @@ function showDiceRoll() {
     whiteOneDice.className = `fa-solid fa-dice-${valueMap[currentDiceRoll.whiteOne]}`;
     whiteTwoDice.className = `fa-solid fa-dice-${valueMap[currentDiceRoll.whiteTwo]}`;
 
+}
+
+function checkAvailableScores() {
+    const whiteDiceValues = []; // To store the values of white dice
+    const coloredDice = {};     // To store the values of colored dice
+
+    // Separate white and colored dice into different arrays/objects
+    Object.keys(currentDiceRoll).forEach((diceName) => {
+        if (diceName.startsWith('white')) {
+            whiteDiceValues.push(currentDiceRoll[diceName]);
+        } else {
+            coloredDice[diceName] = currentDiceRoll[diceName];
+        }
+    });
+
+    // Check the value of turnScoreCount and adjust scoreOptions accordingly
+    Object.keys(coloredDice).forEach((color) => {
+        scoreOptions[color] = [];
+
+        // Initial scoring only includes sum of the white dice
+        if (turnScoreCount === 0) {
+            const sumOfWhites = whiteDiceValues[0] + whiteDiceValues[1];
+            scoreOptions[color].push({
+                [whiteDiceNames[0]]: whiteDiceValues[0],
+                [whiteDiceNames[1]]: whiteDiceValues[1],
+                sum: sumOfWhites
+            });
+        } 
+        
+        // Second scoring includes sum of either white dice and colored dice
+        if (turnScoreCount === 1) {
+            whiteDiceValues.forEach((whiteValue, index) => {
+                scoreOptions[color].push({
+                    [color]: coloredDice[color],
+                    [whiteDiceNames[index]]: whiteValue,
+                    sum: coloredDice[color] + whiteValue
+                });
+            });
+        }
+    });
 }
 
 // Updates existing score buttons
@@ -66,16 +118,14 @@ function updateScoreButtons() {
         const buttons = buttonRow.getElementsByTagName('button'); // Get all buttons in the row
         const availableSums = scoreOptions[color].map(option => option.sum); // Get all available sums for the color
 
-        
         Array.from(buttons).forEach(button => {
             const buttonValue = parseInt(button.getAttribute('value'), 10); // Get the value attribute of the button as an integer
 
-            // FIXME: REFACTOR Make the following conditional segments dryer
             // if turnScoreCount >=2, reset button classes
             if (turnScoreCount >= 2) {
                 button.classList.remove('button-available');
-            } else if (availableSums.includes(buttonValue) && !scoreTrack[color].closed.includes(buttonValue) && !scoreTrack[color].scored.includes(buttonValue)) {
-            // Only do this if button isn't already closed or scored
+            } else if (availableSums.includes(buttonValue) && scoreTrack[color].open.includes(buttonValue)) {
+                // Only do this if button isn't already closed or scored
                 button.classList.add('button-available');
             } else {
                 button.classList.remove('button-available');
@@ -84,38 +134,35 @@ function updateScoreButtons() {
             // If button is closed, show it as closed
             if (scoreTrack[color].closed.includes(buttonValue)) {
                 button.classList.add('button-closed');
-            } else {
-                button.classList.remove('button-closed');
             }
             
             // If button is scored, show it as scored
             if (scoreTrack[color].scored.includes(buttonValue)) {
                 button.classList.add('button-scored');
-            } else {
-                button.classList.remove('button-scored');            
             }
 
             // Lock Button Handling
-            if (!buttonValue) {
+            if (!buttonValue) { // lock buttons have no buttonValue
                 button.classList.add('button-closed');
             } 
             
-            if (!buttonValue && scoreTrack[color].scored.length >= 5 && scoreTrack[color].open.length === 0) {
+            // lock button is active when 5 or more buttons have been scored and there are no open buttons left
+            if (!buttonValue && scoreTrack[color].scored.length >= 5 && scoreTrack[color].open.length === 0 && activeDice[color] === true) {
                 button.classList.remove('button-closed');
+                button.classList.add('button-available');
             }
-
         });
     });
 
     // Enables/Disables Pass Button
     if (turnScoreCount < 2) {
         passBtn.classList.remove('button-disabled');
-        passBtn.disabled = false;
+        rollDiceBtn.classList.add('button-disabled');
     }
 
     if (turnScoreCount >= 2) {
         passBtn.classList.add('button-disabled');
-        passBtn.disabled = true;
+        rollDiceBtn.classList.remove('button-disabled');
     }
 }
 
@@ -126,56 +173,7 @@ function initiateTurn() {
     turnRoll();
     showDiceRoll();
     checkAvailableScores();
-    // populates buttons with score options
     updateScoreButtons();
-}
-
-// rolls all active dice
-function turnRoll() {
-    for (diceColor in activeDice) {
-        if (activeDice[diceColor]) {
-            currentDiceRoll[diceColor] = rollDice();
-        }
-    };
-};
-
-// Marks score
-function markScore(score) {
-    if (turnScoreCount >= 2) {
-        // alert to roll dice again
-        alert('roll the dice again!');
-    } else {
-        
-        const color = score.attributes.color.value;
-        const value = Number(score.value);
-        
-        // Check if the score is already scored
-        if (scoreTrack[color].scored.includes(value)) {
-            console.log('already scored');
-            return;
-        }
-        
-        // Check if the score is closed
-        if (scoreTrack[color].closed.includes(value)) {
-            console.log('cannot score this one');
-            return;
-        }
-        
-        // Check if the score is open and can be scored
-        if (scoreTrack[color].open.includes(value)) {
-            // console.log('scored');
-            // Remove the value from 'open'
-            scoreTrack[color].open = scoreTrack[color].open.filter(item => item !== value);
-            // Add the value to 'scored'
-            scoreTrack[color].scored.push(value);
-        }
-        turnScoreCount++;
-        scoredThisTurn++;
-        // console.log('current scoreTrack:', scoreTrack);
-        closeScores(score);
-        checkAvailableScores();
-        updateScoreButtons();
-    }
 }
 
 // Closes values less than all scored, unless marked as scored
@@ -212,62 +210,42 @@ function closeScores(score) {
 
     // Add these values to 'closed'
     scoreTrack[color].closed.push(...valuesToClose);
-
-    // console.log(`Closed values left of ${scoreValue} for ${color}`);
-
 }
 
-function checkAvailableScores() {
-    const whiteDiceNames = [];  // To store the names of white dice
-    const whiteDiceValues = []; // To store the values of white dice
-    const coloredDice = {};     // To store the values of colored dice
-
-    // Separate white and colored dice into different arrays/objects
-    Object.keys(currentDiceRoll).forEach((diceName) => {
-        if (diceName.startsWith('white')) {
-            whiteDiceNames.push(diceName);
-            whiteDiceValues.push(currentDiceRoll[diceName]);
-        } else {
-            coloredDice[diceName] = currentDiceRoll[diceName];
+// Marks score
+function markScore(score) {
+    if (turnScoreCount < 2) {
+        
+        const color = score.attributes.color.value;
+        const value = Number(score.value);
+        
+        // Check if the score is already scored
+        if (scoreTrack[color].scored.includes(value)) {
+            console.log('already scored');
+            return;
         }
-    });
-
-    // Check the value of turnScoreCount and adjust scoreOptions accordingly
-    Object.keys(coloredDice).forEach((color) => {
-        scoreOptions[color] = [];
-
-        if (turnScoreCount === 0) {
-            // Only include the sum of the white dice
-            if (whiteDiceValues.length === 2) {
-                const sumOfWhites = whiteDiceValues[0] + whiteDiceValues[1];
-                scoreOptions[color].push({
-                    [whiteDiceNames[0]]: whiteDiceValues[0],
-                    [whiteDiceNames[1]]: whiteDiceValues[1],
-                    sum: sumOfWhites
-                });
-            } else if (whiteDiceValues.length === 1) {
-                scoreOptions[color].push({
-                    [whiteDiceNames[0]]: whiteDiceValues[0],
-                    sum: whiteDiceValues[0]
-                });
-            }
-        } else if (turnScoreCount === 1) {
-            // Exclude the sum of the white dice
-            whiteDiceValues.forEach((whiteValue, index) => {
-                scoreOptions[color].push({
-                    [color]: coloredDice[color],
-                    [whiteDiceNames[index]]: whiteValue,
-                    sum: coloredDice[color] + whiteValue
-                });
-            });
-        } else {
-            // console.error('Error: turnScoreCount is greater than one');
+        
+        // Check if the score is closed
+        if (scoreTrack[color].closed.includes(value)) {
+            console.log('cannot score this one');
+            return;
         }
-    });
-
-    // console.log('scoreOptions:', scoreOptions);
-    return scoreOptions;
-
+        
+        // Check if the score is open and can be scored
+        if (scoreTrack[color].open.includes(value)) {
+            // console.log('scored');
+            // Remove the value from 'open'
+            scoreTrack[color].open = scoreTrack[color].open.filter(item => item !== value);
+            // Add the value to 'scored'
+            scoreTrack[color].scored.push(value);
+        }
+        turnScoreCount++;
+        scoredThisTurn++;
+        // console.log('current scoreTrack:', scoreTrack);
+        closeScores(score);
+        checkAvailableScores();
+        updateScoreButtons();
+    }
 }
 
 // Handles Passing on a Score
@@ -279,6 +257,16 @@ function handlePass() {
     // if no points were scored this turn, player gets a penalty
     if (turnScoreCount >= 2 && scoredThisTurn === 0) {
         penaltyCount++;
+    }
+
+    // Add a penalty mark in the DOM
+    for (let i = 0; i < penaltyCount; i++ ) {
+        const penaltyBox = document.getElementById(`penalty-count-box-${i+1}`);
+        penaltyBox.innerHTML = '<i class="fa-solid fa-xmark">';
+    }
+
+    if (penaltyCount >= 4) {
+        endGame();
     }
 }
 
@@ -294,26 +282,77 @@ function setAttributes(element, attributes) {
     };
 };
 
-function chooseScore(chosenColor, chosenIndex) {
-    // Get the chosen score object from the color and index
-    const chosenScore = scoreOptions[chosenColor][chosenIndex];
+function endGame() {
+    const penaltyScoreBox = document.getElementById('score-box-penalty')
+    const totalScoreBox = document.getElementById('score-box-total');
 
-    // Extract the dice used in the chosen score, excluding the 'sum' key
-    const usedDice = Object.keys(chosenScore).filter(key => key !== 'sum');
+    // Game Buttons are hidden
+    // New Game Button Appears
+    startGameBtn.classList.remove('hidden');
+    rollDiceBtn.classList.add('hidden');
+    passBtn.classList.add('hidden');
 
-    // Iterate over each color in scoreOptions
-    for (let color in scoreOptions) {
-        // Filter out objects that use any of the dice in the chosen score
-        scoreOptions[color] = scoreOptions[color].filter(scoreObj => {
-            // Check if this score object uses any of the same dice
-            return !usedDice.some(dice => scoreObj.hasOwnProperty(dice));
-        });
-    }
+    const gameScore = {
+        total: 0,
+    };
 
-    console.log(`Score of ${chosenScore.sum} chosen with dice ${usedDice.join(', ')}.`);
-    return chosenScore.sum;  // Return the sum of the chosen score
+    // Score is calculated
+    // for each color...
+    Object.keys(scoreTrack).forEach((diceColor) => {
+        const scoreBox = document.getElementById(`score-box-${diceColor}`);
+        // Score value Map from game instructions
+        const scoreValueMap = {
+            1: 1,
+            2: 3,
+            3: 6,
+            4: 10,
+            5: 15,
+            6: 21,
+            7: 28,
+            8: 36,
+            9: 45,
+            10: 55,
+            11: 66,
+            12: 78,
+        };
+
+        // number of scored is tallied
+        let scoreTally = scoreTrack[diceColor].scored.length;
+        // tally is mapped to a score value
+        gameScore[diceColor] = scoreValueMap[scoreTally] || 0;
+        gameScore.total += gameScore[diceColor];
+
+        // Score is displayed
+        scoreBox.textContent = gameScore[diceColor];
+    });
+
+    // console.log('gameScore:', JSON.stringify(gameScore, null, 2)); // Pretty print the object
+
+    // Calculate Penalties
+    const penaltyScore = penaltyCount * 5;
+    penaltyScoreBox.textContent = penaltyScore;
+
+    // Calculate Total Score
+    gameScore.total -= penaltyScore;
+    console.log('gameScore.total:', gameScore.total);
+    totalScoreBox.textContent = gameScore.total;
 }
 
+function checkForEndGame() {
+    let closedDice = 0;
+
+    // Loop through activeDice Object
+    Object.keys(activeDice).forEach((diceColor) => {
+        if (!activeDice[diceColor]) {
+            closedDice++;
+        }
+    })
+
+    if (closedDice >=2) {
+        endGame();
+    }
+}
+    
 // Removes color from activeDice object
 function closeColor(e) {
     const color = e.target.attributes.color.value;
@@ -323,6 +362,11 @@ function closeColor(e) {
     setAttributes(inactiveDice, {
         style: 'color: #808080',
     });
+
+    updateScoreButtons();
+
+    // Check for end game conditions
+    checkForEndGame();
 }
 
 // Event Listeners
--- start-game.js
+++ start-game.js
@@ -1,7 +1,52 @@
 // DOM element definition
 const startGameBtn = document.getElementById('start-game-button');
 const passBtn = document.getElementById('pass-button');
+const penaltyTitle = document.getElementById('penalty-title');
+const scoreTitle = document.getElementById('score-title');
+const penaltyCountContainer = document.getElementById('penalty-count-container');
 const scoreRowContainer = document.getElementById('score-row-container');
+
+function resetDOM() {
+    // reset score button rows
+    gameContainer.innerHTML = ''; 
+
+    // reset dice
+    diceContainer.innerHTML = 
+    `
+    <i class="fa-solid fa-dice-one" style="color: #000000;" id="dice-white-one"></i>
+    <i class="fa-solid fa-dice-one" style="color: #000000;" id="dice-white-two"></i>
+    <i class="fa-solid fa-dice-one" style="color: #ff0000;" id="dice-red"></i>
+    <i class="fa-solid fa-dice-one" style="color: #008000;" id="dice-green"></i>
+    <i class="fa-solid fa-dice-one" style="color: #0000ff;" id="dice-blue"></i>
+    <i class="fa-solid fa-dice-one" style="color: #ffff00;" id="dice-yellow"></i>
+    `
+
+    // reset penalty section
+    penaltyCountContainer.innerHTML = 
+    `            
+    <div class="penalty-count-box" id="penalty-count-box-1"></i></div>
+    <div class="penalty-count-box" id="penalty-count-box-2"></div>
+    <div class="penalty-count-box" id="penalty-count-box-3"></div>
+    <div class="penalty-count-box" id="penalty-count-box-4"></div>
+    `;
+
+    // reset total score section
+    scoreRowContainer.innerHTML = 
+    `
+    <div class="score-box border-red" id="score-box-red"></div>
+    <i class="fa-solid fa-plus"></i>
+    <div class="score-box border-yellow" id="score-box-yellow"></div>
+    <i class="fa-solid fa-plus"></i>
+    <div class="score-box border-green" id="score-box-green"></div>
+    <i class="fa-solid fa-plus"></i>
+    <div class="score-box border-blue" id="score-box-blue"></div>
+    <i class="fa-solid fa-minus"></i>
+    <div class="score-box" id="score-box-penalty"></div>
+    <i class="fa-solid fa-equals"></i>
+    <div class="score-box" id="score-box-total"></div>
+    `
+
+}
 
 // Resets global variables for new game
 function initializeGame() {
@@ -15,6 +60,7 @@ function initializeGame() {
         whiteTwo: true,
     }
 
+    resetDOM();
     initializeScoreTrack();
     revealGame();
     displayScoreTrack();
@@ -56,6 +102,11 @@ function revealGame() {
     rollDiceBtn.classList.remove('hidden');
     passBtn.classList.remove('hidden');
     diceContainer.classList.remove('hidden');
+    penaltyTitle.classList.remove('hidden');
+    penaltyCountContainer.classList.remove('hidden');
+    scoreTitle.classList.remove('hidden');
+    scoreRowContainer.classList.remove('hidden');
+
     // populates DOM with buttons for choosing score
     gameColors.forEach(color => {
         const gameRow = document.createElement('div')
@@ -76,7 +127,6 @@ function displayScoreTrack() {
         scoreTrack[color].open.forEach(option => {
             const button = document.createElement('button');
             button.textContent = option;
-            button.className = 'circle-button';
             button.classList.add('score-button');
             setAttributes(button, {
                 value: option,
